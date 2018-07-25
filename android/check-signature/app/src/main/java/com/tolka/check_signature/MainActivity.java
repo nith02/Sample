9970c507df48bba8d113ba64a3b38ad0001642a2
nith.hou <nith.hou@tolka.com.tw>
2018-07-26 00:34:52 +0800

    Implement check-signature

--- android/check-signature/app/src/main/java/com/tolka/check_signature/MainActivity.java
+++ android/check-signature/app/src/main/java/com/tolka/check_signature/MainActivity.java
@@ -1,7 +1,10 @@
 package com.tolka.check_signature;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -11,5 +14,18 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
+
+        try
+        {
+            Signature[] sigs;
+            sigs = this.getPackageManager()
+                    .getPackageInfo( this.getPackageName(), PackageManager.GET_SIGNATURES )
+                    .signatures;
+            Log.e( "nith", "sig = " + sigs.hashCode() );
+        }
+        catch ( PackageManager.NameNotFoundException e )
+        {
+            e.printStackTrace();
+        }
     }
 }
