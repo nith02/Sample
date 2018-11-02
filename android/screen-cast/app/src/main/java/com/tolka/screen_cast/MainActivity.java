5f74201d8b78de67a1c0c63e9fe54adc27351be5
Nith Hou <nith.hou@tolka.com.tw>
2018-11-02 16:50:30 +0800

    Implement screen cast detect

--- android/screen-cast/app/src/main/java/com/tolka/screen_cast/MainActivity.java
+++ android/screen-cast/app/src/main/java/com/tolka/screen_cast/MainActivity.java
@@ -1,9 +1,12 @@
 package com.tolka.screen_cast;
 
+import android.content.Context;
+import android.media.MediaRouter;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity
@@ -23,7 +26,16 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick( View view )
             {
+                Log.i( "nith", "isScreenCasting " + isScreenCasting( MainActivity.this ) );
             }
         } );
     }
+
+    private boolean isScreenCasting( Context context )
+    {
+        MediaRouter mediaRouter = (MediaRouter) context.getSystemService( Context.MEDIA_ROUTER_SERVICE );
+        MediaRouter.RouteInfo info = mediaRouter.getDefaultRoute();
+        return info.getPresentationDisplay() != null;
+    }
+
 }
