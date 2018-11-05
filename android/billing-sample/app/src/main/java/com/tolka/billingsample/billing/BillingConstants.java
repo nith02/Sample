f1062ed54187e8b984f9f0611a13e1c702005919
Nith Hou <nith.hou@tolka.com.tw>
2018-11-05 17:56:40 +0800

    Reformat codes

--- android/billing-sample/app/src/main/java/com/tolka/billingsample/billing/BillingConstants.java
+++ android/billing-sample/app/src/main/java/com/tolka/billingsample/billing/BillingConstants.java
@@ -17,13 +17,15 @@ package com.example.billingmodule.billing;
 
 import com.android.billingclient.api.BillingClient;
 import com.android.billingclient.api.BillingClient.SkuType;
+
 import java.util.Arrays;
 import java.util.List;
 
 /**
  * Static fields and methods useful for billing
  */
-public final class BillingConstants {
+public final class BillingConstants
+{
     // SKUs for our products: the premium upgrade (non-consumable) and gas (consumable)
     public static final String SKU_PREMIUM = "premium";
     public static final String SKU_GAS = "gas";
@@ -35,14 +37,17 @@ public final class BillingConstants {
     private static final String[] IN_APP_SKUS = {SKU_GAS, SKU_PREMIUM};
     private static final String[] SUBSCRIPTIONS_SKUS = {SKU_GOLD_MONTHLY, SKU_GOLD_YEARLY};
 
-    private BillingConstants(){}
+    private BillingConstants()
+    {
+    }
 
     /**
      * Returns the list of all SKUs for the billing type specified
      */
-    public static final List<String> getSkuList(@BillingClient.SkuType String billingType) {
-        return (billingType == SkuType.INAPP) ? Arrays.asList(IN_APP_SKUS)
-                : Arrays.asList(SUBSCRIPTIONS_SKUS);
+    public static final List<String> getSkuList( @BillingClient.SkuType String billingType )
+    {
+        return (billingType == SkuType.INAPP) ? Arrays.asList( IN_APP_SKUS )
+                : Arrays.asList( SUBSCRIPTIONS_SKUS );
     }
 }
 
