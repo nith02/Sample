5059746b0edb276deda7f46a9395f0b58718bafd
nith_hou <nith_hou@tutk.com>
2020-01-09 13:51:19 +0800

    refine codes

--- android/rxjava/app/src/main/java/com/tutk/rxjava/MainActivity.kt
+++ android/rxjava/app/src/main/java/com/tutk/rxjava/MainActivity.kt
@@ -22,72 +22,71 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // onNext not emit if in io schedule
-        Observable.create<String> { e ->
+        Observable.create<String> {
             Log.e(TAG, "emit onNext")
-            e.onNext("hello")
-            e.onError(Exception("error1"))
+            it.onNext("hello")
+            it.onError(Exception("error1"))
         }.observeOn(AndroidSchedulers.mainThread())
             .subscribeOn(Schedulers.io())
             .onErrorReturn { "error" }
-            .subscribe { e -> Log.e(TAG, "subscribe1 $e") }
+            .subscribe { Log.e(TAG, "subscribe1 $it") }
 
-        Observable.create<String> { e ->
+        Observable.create<String> {
             Log.e(TAG, "emit onNext")
-            e.onNext("hello")
-            e.onError(Exception("error1"))
+            it.onNext("hello")
+            it.onError(Exception("error1"))
         }.onErrorReturn { "error" }
-            .subscribe { e -> Log.e(TAG, "subscribe2 $e") }
+            .subscribe { Log.e(TAG, "subscribe2 $it") }
 
         Observable.just("hello")
-            .map { e -> "$e world" }
-            .subscribe { e -> Log.e(TAG, "subscribe3 $e") }
+            .map { "$it world" }
+            .subscribe { Log.e(TAG, "subscribe3 $it") }
 
         Observable.just(1, 2, 3, 4, 5)
-            .flatMap { e -> getData(e).delay(100, TimeUnit.MILLISECONDS) }
-            .subscribe { e -> Log.e(TAG, "subscribe4 $e") }
+            .flatMap { getData(it).delay(100, TimeUnit.MILLISECONDS) }
+            .subscribe { Log.e(TAG, "subscribe4 $it") }
 
         // concatMap process next item after onComplete emitted
         Observable.just(1, 2, 3, 4, 5)
-            .filter { e -> e in 1..3 }
-            .concatMap { e -> getData(e).delay(300, TimeUnit.MILLISECONDS) }
-            .subscribe { e -> Log.e(TAG, "subscribe5 $e") }
+            .filter { it in 1..3 }
+            .concatMap { getData(it).delay(300, TimeUnit.MILLISECONDS) }
+            .subscribe { Log.e(TAG, "subscribe5 $it") }
 
         Observable.zip(
             Observable.just("cat", "cat1", "cat2", "cat3"),
             Observable.just("dog", "dog1", "dog2"),
             BiFunction<String, String, String> { e1, e2 -> "$e1 $e2" })
-            .subscribe { e -> Log.e(TAG, "subscribe6 $e") }
+            .subscribe { Log.e(TAG, "subscribe6 $it") }
 
         val ob1 = getIndexFromServer()
-        val ob2 = ob1.flatMap { e -> getNameFromServer(e) }
+        val ob2 = ob1.flatMap { getNameFromServer(it) }
 
         // ob1 do twice
         Observable.merge(ob1, ob2)
-            .subscribe { e -> Log.e(TAG, "subscribe7 $e") }
+            .subscribe { Log.e(TAG, "subscribe7 $it") }
 
-        ob1.publish { e ->
-            val e2 = e.flatMap { index -> getNameFromServer(index) }
-            Observable.merge(e, e2)
-        }.subscribe { e -> Log.e(TAG, "subscribe8 $e") }
+        ob1.publish {
+            val ob2 = it.flatMap { index -> getNameFromServer(index) }
+            Observable.merge(it, ob2)
+        }.subscribe { Log.e(TAG, "subscribe8 $it") }
 
     }
 
-    fun getData(i: Int) = Observable.create<String> { e ->
-        Log.e(TAG, "getData $i")
-        e.onNext("data $i")
-        e.onComplete()
+    fun getData(i: Int) = Observable.create<String> {
+        it.onNext("data $i")
+        it.onComplete()
     }
 
-    fun getIndexFromServer() = Observable.create<Int> { e ->
+    fun getIndexFromServer() = Observable.create<Int> {
         Log.e(TAG, "getIndexFromServer")
-        e.onNext(7)
-        e.onComplete()
+        it.onNext(7)
+        it.onComplete()
     }
 
-    fun getNameFromServer(i: Int) = Observable.create<String> { e ->
+    fun getNameFromServer(i: Int) = Observable.create<String> {
         Log.e(TAG, "getNameFromServer $i")
-        e.onNext("name $i")
-        e.onComplete()
+        it.onNext("name $i")
+        it.onComplete()
     }
 
 }
